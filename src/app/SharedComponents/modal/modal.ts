319e4a8a599fd6b3b550eb3f9da2d9b0eb6a144f
ygsorianochiukim <ygsoriano@mail.corpmail.info>
2025-12-30 16:20:25 +0800

    Update Changes

--- src/app/SharedComponents/modal/modal.ts
+++ src/app/SharedComponents/modal/modal.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { LucideAngularModule, XCircle } from 'lucide-angular';
 
 @Component({
   selector: 'app-modal',
+  standalone: true,
   imports: [LucideAngularModule],
   templateUrl: './modal.html',
-  styleUrl: './modal.scss',
+  styleUrls: ['./modal.scss'],
 })
 export class Modal {
   readonly CloseIcon = XCircle;
 
+  @Input() isVisible = false;
+  @Input() title = 'Modal Title';
+
+  @Output() close = new EventEmitter<void>();
+  onClose() {
+    this.close.emit();
+  }
 }
--- src/app/views/issues/issues.ts
+++ src/app/views/issues/issues.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { LucideAngularModule, Plus } from 'lucide-angular';
 import { Modal } from '../../SharedComponents/modal/modal';
 
 @Component({
   selector: 'app-issues',
-  imports: [LucideAngularModule , Modal],
+  imports: [LucideAngularModule , Modal , FormsModule],
   templateUrl: './issues.html',
   styleUrl: './issues.scss',
 })
 export class Issues {
   readonly PlusIcon = Plus;
+
+  showModal = false;
 }
